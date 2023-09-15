7dee251793c36e1494c94d20eccab11d78846647
QYT_WJ <80615844+qweas3315@users.noreply.github.com>
2023-09-15 11:19:45 +0800

    Update config.py

--- source_python/config/config.py
+++ source_python/config/config.py
@@ -2,15 +2,14 @@
 UPLOAD_PATH = '/home/files/'  # 保存故障文本路径
 
 """MySQL配置"""
-MYSQL_HOST = '8.130.116.227'  # MySQL地址
+MYSQL_HOST = '127.0.0.1'  # MySQL地址
 MYSQL_USERNAME = 'root'  # MySQL用户名
 MYSQL_PASSWORD = '123456'  # MySQL密码
 MYSQL_DATABASE = 'match_database'  # MySQL数据库名
 MYSQL_PORT = 3306  # MySQL端口
 
 """服务端IP"""
-SERVER_IP = 'http://8.130.116.227:5000'
-# SERVER_IP = 'http://127.0.0.1:5000'
+SERVER_IP = 'http://127.0.0.1:5000'
 
 """"JWT密钥"""
 JWT_SECRET_KEY = 'wang'
